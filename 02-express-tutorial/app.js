2edb90a1ba4750471a69ebc735ea44c8ac94c721
DonMatiu <98232499+DonMatiu@users.noreply.github.com>
2024-03-24 19:04:05 -0300

    authorize and logger

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,27 @@
-console.log('Express Tutorial')
+console.clear();
+const express = require('express');
+const data = require('./data');
+const logger = require('./logger');
+const authorize = require('./authorize');
+const path = require('path');
+
+const app = express();
+
+app.use(logger);
+app.use('/api', authorize);
+
+app.get('/', (req, res) => {
+  res.send('Home');
+});
+
+app.get('/about', (req, res) => {
+  res.send('About Page');
+});
+
+app.get('/api/items', (req, res) => {
+  res.status(200).json(data);
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000...');
+});
